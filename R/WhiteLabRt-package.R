fb862380220118b42439f5e2fef0563cb81c3990
Chad Milando <44262671+cmilando@users.noreply.github.com>
2024-07-18 04:23:12 -0400

    Initial commit

---
+++ R/WhiteLabRt-package.R
@@ -0,0 +1,18 @@
+#' The 'WhiteLabRt' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name WhiteLabRt-package
+#' @aliases WhiteLabRt
+#' @useDynLib WhiteLabRt, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.6. https://mc-stan.org
+#'
+NULL
---
+++ README.md
@@ -0,0 +1,2 @@
+# WhiteLabRt
+ A collection of functions related to novel methods for estimating Rt, created by the lab of Professor Laura White.
